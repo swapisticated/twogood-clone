66e5c3083a08ac1b7ee50015ed218df12cbe7d5e
Swapnil Sharma <70678703+swapisticated@users.noreply.github.com>
2024-01-17 03:43:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -88,22 +88,22 @@ function cookbookAnimation() {
 }
 cookbookAnimation();
 
-// function loadinganimation() {
-//   gsap.from("#page1 h1", {
-//     y: 100,
-//     opacity: 0,
-//     delay: 0.5,
-//     duration: 0.9,
-//     stagger: 0.3,
-//   });
-//   gsap.from("#page1 #cookbook", {
-//     scale: 0.9,
-//     opacity: 0,
-//     delay: 1.3,
-//     duration: 0.5,
-//   });
-// }
-// loadinganimation();
+function loadinganimation() {
+  gsap.from("#page1 h1", {
+    y: 100,
+    opacity: 0,
+    delay: 0.5,
+    duration: 0.9,
+    stagger: 0.3,
+  });
+  gsap.from("#page1 #cookbook", {
+    scale: 0.9,
+    opacity: 0,
+    delay: 1.3,
+    duration: 0.5,
+  });
+}
+loadinganimation();
 
 function cursorAnimation() {
   document.addEventListener("mousemove", function (dets) {
